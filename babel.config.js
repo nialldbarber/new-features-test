1fc963bac4d1ce2fb646b493124d17b74ab7ae9d
Niall Barber <niall.barber@actual-experience.com>
2019-11-06 10:12:35 +0000

    Initial commit

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+
+module.exports = (api) => {
+  api.cache(true)
+
+  return {
+    presets: ['@babel/preset-env', '@babel/preset-typescript', '@babel/preset-react'],
+    plugins: [
+      '@babel/proposal-class-properties',
+      '@babel/proposal-object-rest-spread',
+      [
+        'babel-plugin-root-import',
+        {
+          rootPathPrefix: '~@',
+          rootPathSuffix: 'src'
+        }
+      ]
+    ]
+  }
+}
